7d11b2a43df2326b72227a6dba2b183735ac5a37
Anurag Pal <anuragpal7754@gmail.com>
2024-10-14 14:03:25 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css'
+function Navbar() {
+ 
+ return (
+   <div className='body'>
+       <div className='Taskmanagement'> This section is tasks</div>
+       <div className='Profile'>This is profile</div>
+   </div>
+ );
+}
+
+export default Navbar;
---
+++ src/Components/Taskm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Taskm() {
+ 
+ return (
+   <div className='body'>
+    <div className='container'>
+        
+    </div>
+   </div>
+ );
+}
+
+export default Taskm;
